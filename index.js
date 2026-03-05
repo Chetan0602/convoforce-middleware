6aa4bf4ced53ca8e543e1adaa6a491bc594172bd
Chetan0602 <95068201+Chetan0602@users.noreply.github.com>
2026-03-05 19:21:11 +0530

    add express json

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 -----------------------------------*/
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({limit: '100mb',extended: true}));
-
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,() => {
